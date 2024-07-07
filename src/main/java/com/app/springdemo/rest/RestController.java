2d44f970c0b8f467e34902218321427fcc9e210e
Uday <budayteja009@gmail.com>
2024-07-07 23:34:16 +0530

    Add spring-core

--- src/main/java/com/app/springdemo/rest/RestController.java
+++ src/main/java/com/app/springdemo/rest/RestController.java
@@ -1,12 +1,16 @@
 package com.app.springdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 public class RestController {
+
+    @Value("${msg}")
+    private String msg;
         @RequestMapping("/hello")
         public String sayHello() {
-            return "Hello World!";
+            return msg;
         }
 }
